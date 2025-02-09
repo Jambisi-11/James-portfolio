42715eb54952c9da6c786596c326e079d11ad1b1
Jambisi-11 <jambis.banji@yahoo.com>
2025-02-10 00:10:36 +0100

    Menu bar developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
-
+import Menu from './Components/MenuBar/Menu'
+import Bg from './assets/Backgground.png'
 const App = () => {
   return (
-    <div className='w-full h-full bg-black'>
-      <div><Header /></div>
-       <div className='bg-orange-500 w-[80vw] h-1000 mt-0 m-auto'>
-        
+    <div style={{ backgroundImage: `url(${Bg})` }} 
+      className="w-full h-full bg-cover bg-center bg-opacity-50"
+    >
+      <Header />
+      <Menu />
+       <div className=' w-[80vw] h-200 mt-0 m-auto border-l-1 border-r-1 border-gray-800'>
+         
       </div>
     </div>
   )
