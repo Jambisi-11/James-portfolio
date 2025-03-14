5b50747ea8944d9ed4364b739ee8af558ee64d69
Jambisi-11 <jambis.banji@yahoo.com>
2025-03-15 00:05:43 +0100

    Third Deploy

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,7 +3,7 @@ import aboutPic from '../../assets/about.png';
 
 const About = () => {
     return (
-        <div className="p-5">
+        <div className="p-4 w-full">
             <div className={`${window.innerWidth < 768 ? 'flex flex-col md:flex-row  gap-8':'flex flex-col md:flex-row mt-4 gap-8'}`}>            
                 {/* Image Section (50%) */}
                 <div className={`${window.innerWidth < 768 ? 'w-full flex justify-center items-center ': 'w-full flex justify-center items-center mt-12'}`}>
@@ -18,8 +18,8 @@ const About = () => {
                 </div>
 
                 {/* Text Section (50%) */}
-                <div className="w-full md:w-1/2 flex justify-center items-center">
-                    <p className="text- leading-relaxed p-4 text-black bg-green-400 rounded-br-[3em] rounded-tl-[3em]" > <em>
+                <div className="w-full flex justify-center items-center">
+                    <p className="text-leading-relaxed p-4 text-black bg-green-400 rounded-br-[3em] rounded-tl-[3em]" > <em>
                         <strong>"Hi, I’m Ajide Olabisi James, </strong>  an intermediate Front-End Developer with expertise in HTML, CSS, JavaScript (beginner), React.js, 
                         Tailwind, and collaborative tools like GitHub. I also have a foundational understanding of UX/UI design, which I’ve cultivated 
                         through hands-on experience and self-directed learning. While I’m not formally trained in design, I’m passionate about creating 
